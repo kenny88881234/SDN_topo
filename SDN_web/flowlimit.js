fa80dc356348cdd9f4aa18012b90443139a21a86
kenny <kenny88881234@gmail.com>
2017-12-01 20:05:13 +0800

    2017.12.01

--- SDN_web/flowlimit.js
+++ SDN_web/flowlimit.js
@@ -1,24 +1,45 @@
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-	alert("success!");
     }
 };
 function processFormData() {
-    var ip1_element = document.getElementsByName('ip1');
-    var ip1 = ip1_element[0].value;
-    var ip2_element = document.getElementsByName('ip2');
-    var ip2 = ip2_element[0].value;
-    var ip3_element = document.getElementsByName('ip3');
-    var ip3 = ip3_element[0].value;
-    var ip4_element = document.getElementsByName('ip4');
-    var ip4 = ip4_element[0].value;
-    console.log('ip : ' + ip1 + '.' + ip2 + '.' + ip3 + '.' + ip4 + ' -> ' + select);
-    if(ip1 != "" && ip2 != "" && ip3 != "" && ip4 != "") {
-	xmlhttp.open("GET", "flowlimit.php", true);
-	xmlhttp.send();
+    var data = [3];
+    var flag = 0;
+    for (i=0;i<3;i++) {
+	data[i] = [5];
     }
-    else {
-	alert("input error");
-    }
+    $.getJSON("topo_data.json", function( json ) {
+        for(i=0;i<json.host.length;i++) {
+            data[Number(json.host[i].port.dpid)][Number(json.host[i].port.port_no)] = json.host[i].ipv4[0];
+        }
+    	var ip1_element = document.getElementsByName('ip1');
+    	var ip1 = ip1_element[0].value;
+    	var ip2_element = document.getElementsByName('ip2');
+    	var ip2 = ip2_element[0].value;
+    	var ip3_element = document.getElementsByName('ip3');
+    	var ip3 = ip3_element[0].value;
+    	var ip4_element = document.getElementsByName('ip4');
+    	var ip4 = ip4_element[0].value;
+    	var select_element = document.getElementsByName('select');
+    	var select = select_element[0].value;
+    	if(ip1 != "" && ip2 != "" && ip3 != "" && ip4 != "") {
+	    for(i=0;i<data.length;i++) {
+	    	for(j=0;j<data[i].length;j++) {
+	    	    if(ip1+'.'+ip2+'.'+ip3+'.'+ip4 == data[i][j]) {
+	    	    	xmlhttp.open("GET", "flowlimit.php?ip1="+ip1+"&ip2="+ip2+"&ip3="+ip3+"&ip4="+ip4+"&select="+select+"&s="+i+"&p="+j, true);
+	    	    	xmlhttp.send();
+		    	flag=1;
+			alert("Success!");
+	    	    }
+	    	}
+	    }
+	    if(flag != 1) {
+	    	alert("Not exist");
+	    }
+    	}
+    	else {
+	    alert("Input error");
+    	}
+    });
 }
--- SDN_web/flowlimit.php
+++ SDN_web/flowlimit.php
@@ -4,16 +4,41 @@ $ip2 = $_GET[ip2];
 $ip3 = $_GET[ip3];
 $ip4 = $_GET[ip4];
 $select = $_GET[select];
-echo $ip1;
+$s = $_GET[s];
+$p = $_GET[p];
+$pn = (int)$p;
+$p = (string)$pn-1;
+
+$myfile = fopen("everydaylimit.json", "r") or die("Unable to open file!");
+$content = '';
+while (!feof($myfile)) {
+    $content .= fread($myfile, filesize("everydaylimit.json"));
+}
+
+$content = json_decode($content);
+fclose($myfile);
+
+$myfile = fopen("everydaylimit.json", "w") or die("Unable to open file!");
+if($select == '0') {
+    $content->$s->$p->flag = '0';
+}
+else {
+    $content->$s->$p->flag = '1';
+}
+$content = json_encode($content);
+
+fwrite($myfile, $content);
+
+fclose($myfile);
+
 $service_url = 'http://192.168.2.105:8080/qos/rules/0000000000000001';
- 
 $curl = curl_init();
 $header[] = "Accept: application/json";
 $header[] = "Accept-Encoding: gzip";
 $header[] = "Content-Type:application/json";
 curl_setopt($curl, CURLOPT_URL, $service_url);
 curl_setopt($curl, CURLOPT_HTTPHEADER, $header );
- 
+
 $curl_post_data = array(
     'priority' => '2',
     'match' => array('nw_dst' => $ip1 . '.' . $ip2 . '.' . $ip3 . '.' . $ip4),
@@ -22,24 +47,25 @@ $curl_post_data = array(
 
 
 $curl_post_data = json_encode($curl_post_data);
- 
+
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_POST, true);
 curl_setopt($curl, CURLOPT_POSTFIELDS, $curl_post_data);
 $curl_response = curl_exec($curl);
- 
+
 if ($curl_response === false) {
     $info = curl_getinfo($curl);
     curl_close($curl);
     die('error occured during curl exec. Additioanl info: ' . var_export($info));
 }
- 
+
 curl_close($curl);
- 
+
 $decoded = json_decode($curl_response);
- 
+
 if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
     die('error occured: ' . $decoded->response->errormessage);
 }
+
 header("Location:flowlimit.html");
 ?>
